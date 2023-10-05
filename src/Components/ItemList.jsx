76225a633a4441847c5991f08df595cc84fccc9a
German Del Castello <gdlcastello@gmail.com>
2023-10-04 21:15:20 -0300

    Entrega final se aplican todas las correciones señaladas

--- src/Components/ItemList.jsx
+++ src/Components/ItemList.jsx
@@ -1,59 +1,29 @@
 import PropTypes from 'prop-types';
-import { Link } from "react-router-dom";
-import './ItemListCard.css';
-
+import { Link } from 'react-router-dom';
+import styles from './ItemList.module.css';
 
 const ItemList = ({ items, isLoading }) => {
     if (isLoading) {
-        return <h2>Loading...</h2>
+        return <h2>Loading...</h2>;
     }
+
     return (
         <div>
             <h1>Productos</h1>
-            <div style={{ display: 'flex', flexWrap: 'wrap' }}> 
+            <div className={`${styles.cardContainer} ${styles.flexContainer}`}>
                 {items.map((item) => (
-                    <div key={item.Id} style={cardStyle}>
-                        <Link to={`/item/${item.Id}`} style={linkStyle}>
-                            <img src={`/${item.imageId}`} alt={item.title} style={imageStyle} />
-                            <h3 style={titleStyle}>{item.title}</h3>
-                            <p style={priceStyle}>${item.price}</p>
-                            <p style={categoryStyle}>{item.categoryId}</p>
+                    <div key={item.Id} className={styles.card}>
+                        <Link to={`/item/${item.Id}`} className={styles.link}>
+                            <img src={`/${item.imageId}`} alt={item.title} className={styles.image} />
+                            <h3 className={styles.title}>{item.title}</h3>
+                            <p className={styles.price}>${item.price}</p>
+                            <p className={styles.category}>{item.categoryId}</p>
                         </Link>
                     </div>
                 ))}
             </div>
         </div>
     );
-};
-
-const cardStyle = {
-    padding: '20px',
-    border: '1px solid #ccc',
-    borderRadius: '10px',
-    margin: '10px',
-    width: '250px',
-};
-
-const linkStyle = {
-    textDecoration: 'none',
-    color: 'black',
-};
-
-const imageStyle = {
-    width: '100%',
-    borderRadius: '10px',
-};
-
-const titleStyle = {
-    margin: '10px 0',
-};
-
-const priceStyle = {
-    fontWeight: 'bold',
-};
-
-const categoryStyle = {
-    color: '#888',
 };
 
 ItemList.propTypes = {
